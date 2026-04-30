6ddd13c16e6f2f45ed56dca81e001f91a35de88b
Jasper Liu <xizliu106@gmail.com>
2026-04-29 17:44:23 -0700

    attributes/methods of the Map implemented (rough draft)

--- WSS/src/wss/map/Map.java
+++ WSS/src/wss/map/Map.java
@@ -1,5 +1,50 @@
 package wss.map;
 
-public class Map {
-    
+import wss.entities.Player;
+import wss.terrain.TerrainSquare;
+
+public class Map 
+{
+    private int height;
+    private int width;
+    private Difficulty difficulty;
+    private TerrainSquare[][] grid;
+    private Player player;
+
+    public Map(int height, int width, Difficulty difficulty) 
+    {
+        this.width = width;
+        this.height = height;
+        this.difficulty = difficulty;
+        this.grid = new TerrainSquare[height][width];
+        // TODO: generate the map based on difficulty
+        // generateMap(difficulty);
+    }
+
+    public int getHeight() 
+    {
+        return height;
+    }
+
+    public int getWidth() 
+    {
+        return width;
+    }
+
+
+    public TerrainSquare getTerrainAt(int y, int x)
+    {
+        if (x >= 0 && x < width && y >= 0 && y < height) 
+            return grid[y][x];
+        else 
+            throw new IndexOutOfBoundsException("Coordinates out of bounds");
+    }
+
+    public void displayMap()
+    {
+        // TODO: implement the display logic of the current map state
+    }
+
+
+
 }
