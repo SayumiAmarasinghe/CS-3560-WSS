ccd81a49d141e2ac921b640fc0c2865cbadbee47
Nicholas Garcia <nickgarcia0523@gmail.com>
2026-05-03 18:35:37 -0700

    Update KeenEyedVision.java

--- WSS/src/wss/vision/KeenEyedVision.java
+++ WSS/src/wss/vision/KeenEyedVision.java
@@ -1,5 +1,21 @@
 package wss.vision;
 
-public class KeenEyedVision {
-    
+public class KeenEyedVision extends Vision {
+    public KeenEyedVision() {
+        super("Keen-Eyed Vision");
+    }
+
+    @Override
+    public int[][] getVisibleOffsets() {
+        return new int[][] {
+            {-1, 0}, //north
+            {1, 0},  //south
+
+            {-1, 1}, //northeast
+            {0, 1},  //east
+            {1, 1},  //southeast
+
+            {0, 2}   //for two squares east
+        };
+    }
 }
