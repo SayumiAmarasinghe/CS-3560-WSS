08e8a086f9d9a573900ed8f02e518b02d1228420
Nicholas Garcia <nickgarcia0523@gmail.com>
2026-05-03 18:33:48 -0700

    Update FocusedVision.java

--- WSS/src/wss/vision/FocusedVision.java
+++ WSS/src/wss/vision/FocusedVision.java
@@ -1,5 +1,16 @@
 package wss.vision;
 
-public class FocusedVision {
-    
+public class FocusedVision extends Vision {
+    public FocusedVision() {
+        super("Focused Vision");
+    }
+
+    @Override
+    public int[][] getVisibleOffsets() {
+        return new int[][] {
+            {-1, 1}, //northeast
+            {0, 1},  //east
+            {1, 1}   //southeast
+        };
+    }
 }
