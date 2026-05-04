6f362796d2e40394ed36a876ba9dd4cb0cf1d7fa
Nicholas Garcia <nickgarcia0523@gmail.com>
2026-05-03 18:32:57 -0700

    Update Vision.java

--- WSS/src/wss/vision/Vision.java
+++ WSS/src/wss/vision/Vision.java
@@ -1,5 +1,34 @@
 package wss.vision;
 
-public class Vision {
-    
-}
+public abstract class Vision {
+    private String name;
+
+    public Vision(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    //Each vision type will decide what squares it can see
+    //Each row is {rowChange, colChange}
+    public abstract int[][] getVisibleOffsets();
+    public boolean canSee(int playerRow, int playerCol, int targetRow, int targetCol) {
+        int rowDifference = targetRow - playerRow;
+        int colDifference = targetCol - playerCol;
+        for (int[] offset : getVisibleOffsets()) {
+            if (offset[0] == rowDifference && offset[1] == colDifference) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public void printVisionScope() {
+        System.out.println(name + " can see these relative squares:");
+
+        for (int[] offset : getVisibleOffsets()) {
+            System.out.println("Row change: " + offset[0] + ", Column change: " + offset[1]);
+        }
+    }
+} 
