6d965da964f5e37f0baeb527bf5abf1dba461154
Nicholas Garcia <nickgarcia0523@gmail.com>
2026-05-03 18:41:28 -0700

    Update Main.java

--- WSS/src/wss/Main.java
+++ WSS/src/wss/Main.java
@@ -2,11 +2,22 @@ package wss;
 import wss.gui.EntryScreen;
 import wss.gui.InstrScreen;
 
+//Vision testing
+import wss.vision.Vision;
+import wss.vision.VisionFactory;
+
 public class Main {
     public static void main(String[] args) 
     {
         EntryScreen entryScreen = new EntryScreen();
         InstrScreen instrScreen = new InstrScreen();
 
+        //Vision test
+        Difficulty difficulty = Difficulty.HARD;
+        System.out.println("Allowed visions on " + difficulty + ":");
+        System.out.println(VisionFactory.getAllowedVisionNames(difficulty));
+        Vision vision = VisionFactory.createVision("Cautious", difficulty);
+        System.out.println("Chosen vision: " + vision.getName());
+        vision.printVisionScope();
     }
 }
