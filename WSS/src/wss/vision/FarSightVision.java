b7392c9489c26fc1485fdfb6a4c62bb088ef6f06
Nicholas Garcia <nickgarcia0523@gmail.com>
2026-05-03 18:36:13 -0700

    Update FarSightVision.java

--- WSS/src/wss/vision/FarSightVision.java
+++ WSS/src/wss/vision/FarSightVision.java
@@ -1,5 +1,29 @@
 package wss.vision;
 
-public class FarSightVision {
-    
+public class FarSightVision extends Vision {
+    public FarSightVision() {
+        super("Far-Sight Vision");
+    }
+
+    @Override
+    public int[][] getVisibleOffsets() {
+        return new int[][] {
+            {-2, 0},
+            {-1, 0},
+            {1, 0},
+            {2, 0},
+
+            {-2, 1},
+            {-1, 1},
+            {0, 1},
+            {1, 1},
+            {2, 1},
+
+            {-2, 2},
+            {-1, 2},
+            {0, 2},
+            {1, 2},
+            {2, 2}
+        };
+    }
 }
