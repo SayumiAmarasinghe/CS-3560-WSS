2c322ff8862a1aa9d62dc33e5c4def868e8fd49c
Nicholas Garcia <nickgarcia0523@gmail.com>
2026-05-03 18:34:22 -0700

    Update CautiousVision.java

--- WSS/src/wss/vision/CautiousVision.java
+++ WSS/src/wss/vision/CautiousVision.java
@@ -1,5 +1,16 @@
 package wss.vision;
 
-public class CautiousVision {
-    
+public class CautiousVision extends Vision {
+    public CautiousVision() {
+        super("Cautious Vision");
+    }
+
+    @Override
+    public int[][] getVisibleOffsets() {
+        return new int[][] {
+            {-1, 0}, //north
+            {1, 0},  //south
+            {0, 1}   //east
+        };
+    }
 }
