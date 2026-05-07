d2f4feef2d8caaa6ee32bfee58c253dbc3b0dac9
Samy <115193685+SamyDuh@users.noreply.github.com>
2026-05-06 18:44:39 -0700

    Brain framework

--- WSS/src/wss/ai/brain/Brain.java
+++ WSS/src/wss/ai/brain/Brain.java
@@ -1,5 +1,21 @@
 package wss.ai.brain;
+import wss.vision.Vision;
 
 public class Brain {
     
+    Vision vision;
+
+    // Constructor
+    Brain(Vision vision){
+        this.vision = vision;
+    }
+
+    // Calls upon the brain to decide on a move to make 
+    public static void MakeMove(){
+        
+    }
+
+    public static void PollBrain(){
+        
+    }
 }
