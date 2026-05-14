1547d93d66c44e866164e54af8c73da3368421db
SamyTheSpheal <lsjimenez363@gmail.com>
2026-05-14 00:13:43 -0700

    Quick item fix

--- WSS/src/wss/items/Item.java
+++ WSS/src/wss/items/Item.java
@@ -4,9 +4,9 @@ import wss.entities.Player;
 
 public abstract class Item {
 
-    protected Boolean isTaken;
+    protected Boolean isTaken = false;
     // Can the item be replenished?
-    protected Boolean repeating;
+    protected Boolean repeating = true;
     
     // Abstract methods to be implemented in item subclasses
     public abstract void replenish(int amount);
